aed9566faf59e159a6eacf9a257d6b99da5179a4
Sasho <gabrovski@thinkpad.(none)>
2012-03-07 00:46:26 -0500

    trying to ow the page fault handler

---
+++ pfault.c
@@ -0,0 +1,25 @@
+#include <linux/security.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/asm/traps.h>
+
+
+
+static struct security_operations pax_mod_sec_ops = {
+  .name = "pax_mod",
+};
+
+dotraplinkage void pax_mod_do_page_fault(struct pt_regs *regs, long error_code) {
+  printk(KERN_INFO "my fault\n");
+  do_page_faul(regs, error_code);
+}
+
+int init_module() { 
+  printk(KERN_INFO "init module pax_mod\n");
+  return 0; 
+}
+
+void cleanup_module() { 
+  return; 
+}
+
